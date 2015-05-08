dd24ed92e7a2d22087d3b1bee40177e64036d2cb
Edward Quach <quach77@gmail.com>
2015-05-07 18:34:02 -0700

    this is my first push

---
+++ src/edu/cpp/cs/cs141/assignment2/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.cpp.cs.cs141.assignment2;
+
+/**
+ * @author EQ iii
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
