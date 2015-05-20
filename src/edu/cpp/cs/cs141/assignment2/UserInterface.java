43f11346fdab0589e34a617fe424805ebb80846e
Edward Quach <quach77@gmail.com>
2015-05-20 09:09:12 -0700

    new class/constructors
    but does not check for overlap

--- src/edu/cpp/cs/cs141/assignment2/UserInterface.java
+++ src/edu/cpp/cs/cs141/assignment2/UserInterface.java
@@ -29,6 +29,16 @@ public class UserInterface {
 	private String[][] map = new String[9][9];
 	private int mapColumn = map.length;
 	int[] roomCoordinate = new int[] { 2, 4, 6 };
+	int[] eLocation0 = new int[2];
+	int[] eLocation1 = new int[2];
+	int[] eLocation2 = new int[2];
+	int[] eLocation3 = new int[2];
+	int[] eLocation4 = new int[2];
+	int[] eLocation5 = new int[2];
+	int[] eLocation6 = new int[2];
+	int[] aB = new int[2];
+	int[] invin = new int[2];
+	int[] rad = new int[2];
 
 	/**
 	 * Handles how the movement will be controlled and the array is being moved
@@ -63,62 +73,69 @@ public class UserInterface {
 	public void setRooms(int i, int j) {
 		if (i == map.length - 1 && j == 0) {
 			map[i][j] = "[S]";
-		} 
-		else if (i == 2 && j == 2)
-			   map[i][j] = "[R]";
-			  else if (i == 2 && j == 2)
-			   map[i][j] = "[R]";
-			  else if (i == 2 && j == 4)
-			   map[i][j] = "[R]";
-			  else if (i == 2 && j == 6)
-			   map[i][j] = "[R]";
-			  else if (i == 4 && j == 2)
-			   map[i][j] = "[R]";
-			  else if (i == 4 && j == 4)
-			   map[i][j] = "[R]";
-			  else if (i == 4 && j == 6)
-			   map[i][j] = "[R]";
-			  else if (i == 6 && j == 2)
-			   map[i][j] = "[R]";
-			  else if (i == 6 && j == 4)
-			   map[i][j] = "[R]";
-			  else if (i == 6 && j == 6)
-			   map[i][j] = "[R]";
-		else if ()
-			map[i][j] = "[A]";
-		else if ()
-			map[i][j] = "[A]";
-		else if ()
-			map[i][j] = "[A]";
-		else if ()
-			map[i][j] = "[A]";
-		else if ()
-			map[i][j] = "[A]";
-		else if ()
-			map[i][j] = "[A]";
-		else if ()
-			map[i][j] = "[I]";
-		else if ()
-			map[i][j] = "[B]";
-		else if ()
+		} else if (i == 2 && j == 2)
 			map[i][j] = "[R]";
+		else if (i == 2 && j == 2)
+			map[i][j] = "[R]";
+		else if (i == 2 && j == 4)
+			map[i][j] = "[R]";
+		else if (i == 2 && j == 6)
+			map[i][j] = "[R]";
+		else if (i == 4 && j == 2)
+			map[i][j] = "[R]";
+		else if (i == 4 && j == 4)
+			map[i][j] = "[R]";
+		else if (i == 4 && j == 6)
+			map[i][j] = "[R]";
+		else if (i == 6 && j == 2)
+			map[i][j] = "[R]";
+		else if (i == 6 && j == 4)
+			map[i][j] = "[R]";
+		else if (i == 6 && j == 6)
+			map[i][j] = "[R]";
+		else if (i == eLocation1[0] && j == eLocation1[1])
+			map[i][j] = "[A]";
+		else if (i == eLocation2[0] && j == eLocation2[1])
+			map[i][j] = "[A]";
+		else if (i == eLocation3[0] && j == eLocation3[1])
+			map[i][j] = "[A]";
+		else if (i == eLocation4[0] && j == eLocation4[1])
+			map[i][j] = "[A]";
+		else if (i == eLocation5[0] && j == eLocation5[1])
+			map[i][j] = "[A]";
+		else if (i == eLocation6[0] && j == eLocation6[1])
+			map[i][j] = "[A]";
+		else if (i == aB[0] && j == aB[1])
+			map[i][j] = "[B]";
+		else if (i == invin[0] && j == invin[1])
+			map[i][j] = "[I]";
+		else if (i == rad[0] && j == rad[1])
+			map[i][j] = "[r]";
 		else {
-//			for (int a = 0; a < roomCoordinate.length; a++) {
-//				if (i == roomCoordinate[a]) {
-//					for (int b = 0; b < roomCoordinate.length; b++) {
-//						if (j == roomCoordinate[b]) {
-//							map[i][j] = "[R]";
-//							break;
-//						}
-//					}
-//				} else
-					map[i][j] = "[*]";
-//			}
+			// for (int a = 0; a < roomCoordinate.length; a++) {
+			// if (i == roomCoordinate[a]) {
+			// for (int b = 0; b < roomCoordinate.length; b++) {
+			// if (j == roomCoordinate[b]) {
+			// map[i][j] = "[R]";
+			// break;
+			// }
+			// }
+			// } else
+			map[i][j] = "[*]";
+			// }
 		}
 	}
 
-
 	public void printMap() {
+		enemyG1();
+		enemyG2();
+		enemyG3();
+		enemyG4();
+		enemyG5();
+		enemyG6();
+		additionalBullet();
+		invincibility();
+		radar();
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[i].length; j++) {
 				setRooms(i, j);
@@ -128,30 +145,76 @@ public class UserInterface {
 		}
 	}
 
-	// will place in enemy class as constructor 
-	public void enemyG1(){
-        int[] eLocation= new int[2];
-        eLocation[0] = position 
-
-	}
-		
-		
-	
-	public boolean positionChecking(int a, int b){
-		if (a == map.length -1 && j == 0){
-			map[i][j] = "[S]";
-		}}
+	// will place in enemy class as constructor
+	public void enemyG1() {
+		positionChecking();
+		eLocation1[0] = eLocation0[0];
+		eLocation1[1] = eLocation0[1];
 	}
 
-	public int ninjaSpawning(int n) {
-		int n = coordinateGenerater();
-		if (n >= map.length - 4) {
-			if (n <= 5) {
-				ninjaSpawning();
-			}
+	public void enemyG2() {
+		positionChecking();
+		eLocation2[0] = eLocation0[0];
+		eLocation2[1] = eLocation0[1];
+	}
+
+	public void enemyG3() {
+		positionChecking();
+		eLocation3[0] = eLocation0[0];
+		eLocation3[1] = eLocation0[1];
+	}
+
+	public void enemyG4() {
+		positionChecking();
+		eLocation4[0] = eLocation0[0];
+		eLocation4[1] = eLocation0[1];
+	}
+
+	public void enemyG5() {
+		positionChecking();
+		eLocation5[0] = eLocation0[0];
+		eLocation5[1] = eLocation0[1];
+	}
+
+	public void enemyG6() {
+		positionChecking();
+		eLocation6[0] = eLocation0[0];
+		eLocation6[1] = eLocation0[1];
+	}
+
+	public void positionChecking() {
+		int a = coordinateGenerater();
+		int b = coordinateGenerater();
+		if (a <= map.length - 4) {
+			eLocation0[0] = a;
+			eLocation0[1] = b;
+		} else if (a > map.length - 4) {
+			if (b <= 5) {
+				positionChecking();
+			} else
+				eLocation0[1] = b;
 		}
-		return n;
 	}
+
+	public void additionalBullet() {
+		aB[0] = coordinateGenerater();
+		aB[1] = coordinateGenerater();
+	}
+
+	public void invincibility() {
+		invin[0] = coordinateGenerater();
+		invin[1] = coordinateGenerater();
+	}
+
+	public void radar() {
+		rad[0] = coordinateGenerater();
+		rad[1] = coordinateGenerater();
+	}
+
+	// public int ninjaSpawning(int n) {
+	// int n = coordinateGenerater();
+	// return n;
+	// }
 
 	public int coordinateGenerater() {
 		int number;
